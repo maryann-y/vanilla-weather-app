3077204ab8452b8c74f44fe78720e6bc3466590f
maryann-y <ma.yeboah6@gmail.com>
2022-10-28 16:44:53 +0100

    Removed some HTML and added it to the JS file

--- src/app.js
+++ src/app.js
@@ -23,6 +23,39 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Sat", "Sun", "Mon", "Tues"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+          <div class="col-2">
+           <div class="weather-forecast-date">${day}</div>
+            <img
+             src="https://openweathermap.org/img/wn/01d@2x.png" 
+             alt=""
+             width="42" 
+             />
+            <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max"> 18° </
+            span>
+            <span class="weather-forecast-temperature-min"> 12° </
+            span>
+            </div>
+          </div>
+         
+          `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -88,3 +121,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+displayForecast();
